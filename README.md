b16169752691e5d43ef21c543832d3130fe539a2
Diego <songecko@gmail.com>
2016-11-28 21:23:55 -0300

    =admin improvements

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Symfony Standard Edition
+Odiseo Standard Edition
 ========================
 
 Welcome to the Symfony Standard Edition - a fully-functional Symfony
--- src/Odiseo/Bundle/AppBundle/Entity/User.php
+++ src/Odiseo/Bundle/AppBundle/Entity/User.php
@@ -6,10 +6,11 @@ use DateTime;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
+use Sylius\Component\Resource\Model\ResourceInterface;
 
 /**
  * User
  */
-class User extends BaseUser
+class User extends BaseUser implements ResourceInterface
 {
 }
