f135a1ef7905a611d74c067d87dc08a3ba7e220c
¨PabloGit88¨ <pablo.damico88@gmail.com>
2016-12-19 16:58:19 -0300

    init gulp

---
+++ Gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var chug = require('gulp-chug');
+var argv = require('yargs').argv;
+
+config = [
+    '--rootPath',
+    argv.rootPath || '../../../web/assets/',
+    '--nodeModulesPath',
+    argv.nodeModulesPath || '../../../node_modules/'
+];
+
+gulp.task('admin', function() {
+    gulp.src('vendor/odiseoteam/odiseo-backend-bundle/Gulpfile.js', { read: false })
+        .pipe(chug({ args: config }))
+    ;
+});
+
+
+gulp.task('default', ['admin']);
