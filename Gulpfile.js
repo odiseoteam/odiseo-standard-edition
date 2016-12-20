4ee54d46b903f1bea5c2f20951f1840fe5909022
Diego <songecko@gmail.com>
2016-12-20 14:21:29 -0300

    =fixes

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,17 +2,29 @@ var gulp = require('gulp');
 var chug = require('gulp-chug');
 var argv = require('yargs').argv;
 
-config = [
+config_admin = [
     '--rootPath',
     argv.rootPath || '../../../web/assets/',
     '--nodeModulesPath',
     argv.nodeModulesPath || '../../../node_modules/'
 ];
 
+config_app = [
+    '--rootPath',
+    argv.rootPath || '../../../../web/assets/',
+    '--nodeModulesPath',
+    argv.nodeModulesPath || '../../../../node_modules/'
+];
+
 gulp.task('admin', function() {
     gulp.src('vendor/odiseoteam/odiseo-backend-bundle/Gulpfile.js', { read: false })
-        .pipe(chug({ args: config }))
+        .pipe(chug({ args: config_admin }))
     ;
 });
 
-gulp.task('default', ['admin']);
+gulp.task('app', function() {
+    gulp.src('src/Odiseo/Bundle/AppBundle/Gulpfile.js', { read: false })
+        .pipe(chug({ args: config_app }))
+    ;
+});
+gulp.task('default', ['admin', 'app']);
